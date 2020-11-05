2c07defa7a604164f610c6712e139241c67ef6d4
shinya-dtn <majin.beesan@gmail.com>
2020-11-05 15:09:40 +0900

    deviseのviewファイルを再作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.parmit(:sign_up, keys: [:nickname, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday])
   end
 end
