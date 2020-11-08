098bf3dbfbe3cfb1a780d3b7f6364fa4e521b38d
shinya-dtn <majin.beesan@gmail.com>
2020-11-08 12:35:20 +0900

    userモデルのアソシエーションとバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :events
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :birthday
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_10_31_081531) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
-    t.date "birhday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
